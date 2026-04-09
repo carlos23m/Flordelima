63bf55cd11dde24db95f9f165234ebbcf1d1c485
Carlos Mendez <carlos129413@gmail.com>
2026-04-08 18:43:59 -0600

    Fix white screen: remove Chakra UI and use simple React components

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-      <Analytics />
-      <SpeedInsights />
-    </ChakraProvider>
+    <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
